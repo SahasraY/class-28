7912195fa6011a5eae860413877af8da75485d0e
SahasraY <yamini1013@gmail.com>
2020-11-27 16:52:56 +0530

    modified

--- Slingshot.js
+++ Slingshot.js
@@ -1,20 +1,26 @@
-class Chain{
-    constructor(bodyA, bodyB){
+class Slingshot{
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
-            stiffness: 0.04,
-            length: 10
+            pointB:pointB,
+            stiffness:0.7,
+            length: 50
         }
         this.chain = Constraint.create(options);
+        this.pointB=pointB
         World.add(world, this.chain);
+    }
+    fly(){
+        this.chain.bodyA=null
     }
 
     display(){
+        if(this.chain.bodyA){
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.pointB
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
     }
     
 }
